64717f53c4908b0228717c08009bb79234c42674
pratik-hanmante <pratikhanmante3@gmail.com>
2024-01-01 23:52:40 +0530

    routing for auth pages register,signin

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import { BsSearch } from "react-icons/bs";
 
 
 const Navbar = () => {
+
+  const user = true;
   return (
     <div className='flex items-center justify-between px6 md:px-[200px] py-4'>
         <h1 className="text-4xl font-semibold tracking-tight text-gray-800 transition duration-300 ease-in-out transform hover:text-blue-500 font-sans">
@@ -23,8 +25,8 @@ const Navbar = () => {
 </div>
 
         <div className="flex items-center justify-center space-x-4 md:space-x-4">
-            <h3><Link to='/login'>Login</Link></h3>
-            <h3><Link to='/register'>Register</Link></h3>
+           {user ? <h3><Link to='/write'>Write</Link></h3>:<h3><Link to='/login'>Login</Link></h3>}
+           {user ? <h3>Profile</h3>:<h3><Link to='/register'>Register</Link></h3>}
         </div>
     </div>
   )
